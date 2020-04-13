935dc3974b25d2a51f8d997e3972f1fe5afa59f5
Jon Brennecke <jpbrennecke@gmail.com>
2020-04-13 12:44:18 -0700

    barkd and laserlike case study sections

--- packages/components/src/components/index.ts
+++ packages/components/src/components/index.ts
@@ -5,9 +5,12 @@ export * from './Heading';
 // logos
 export * from './OpenGovLogo';
 export * from './OpenGovEmblem';
+export * from './LaserlikeLogo';
+export * from './BarkdLogo';
 
 // UI
 export * from './CaseStudyCard';
 export * from './ViewButton';
 export * from './BrowserFrame';
+export * from './PhoneFrame';
 export * from './Tag';
