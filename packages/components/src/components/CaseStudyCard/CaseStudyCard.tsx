22fa2bcffdf03ddeab4afc516577d5dbb86d7829
Jon Brennecke <jpbrennecke@gmail.com>
2020-04-14 17:14:43 -0700

    phone frame

--- packages/components/src/components/CaseStudyCard/CaseStudyCard.tsx
+++ packages/components/src/components/CaseStudyCard/CaseStudyCard.tsx
@@ -59,11 +59,18 @@ const darkerBorderShadowCss = css`
 const ImageContent = styled.div`
   display: flex;
   flex: 1;
+  width: 100%;
+  height: 100%;
   transition: 250ms transform ease-in-out, 250ms filter ease-in-out;
   ${borderShadowCss}
 
   & > * {
+    position: absolute;
+    top: 50%;
+    bottom: 0;
     width: 100%;
+    max-height: 100%;
+    transform: translateY(-50%);
   }
 
   &:first-child {
@@ -91,6 +98,7 @@ const Container = styled.div`
   perspective: 1500px;
   border-radius: ${unit}px;
   padding: ${unit * 5}px ${unit * 7}px;
+  height: 525px;
 
   &:hover {
     ${ImageContent}:first-child {
@@ -126,7 +134,7 @@ interface CaseStudyCardSubComponents {
 export const CaseStudyCard: React.FC<CaseStudyCardProps> &
   CaseStudyCardSubComponents = ({
   className,
-  children
+  children,
 }: CaseStudyCardProps) => (
   <Container className={className}>{children}</Container>
 );
