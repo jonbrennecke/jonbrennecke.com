cc35bd604000d31f75cc5108454612fcd1c4bd2d
Jon Brennecke <jpbrennecke@gmail.com>
2020-04-15 20:17:18 -0700

    form ui

--- packages/components/src/components/Form/Form.stories.tsx
+++ packages/components/src/components/Form/Form.stories.tsx
@@ -8,5 +8,23 @@ storiesOf('UI', module)
   .add('Form', () => (
     <Form>
       <Form.Title>Let's talk!</Form.Title>
+      <Form.Note>
+        Want to chat? Shoot me an email at{' '}
+        <a href="mailto:jpbrennecke@gmail.com" target="_blank">
+          jpbrennecke@gmail.com
+        </a>
+      </Form.Note>
+      <Form.Field>
+        <Form.Label>Name & Company</Form.Label>
+        <Form.Input placeholder="John from Google..." />
+      </Form.Field>
+      <Form.Field>
+        <Form.Label>Email</Form.Label>
+        <Form.Input placeholder="john@google.com" />
+      </Form.Field>
+      <Form.Field>
+        <Form.Label>Project Description</Form.Label>
+        <Form.Input placeholder="We need help to redesign our app..." />
+      </Form.Field>
     </Form>
   ));
--- packages/components/src/components/Form/Form.tsx
+++ packages/components/src/components/Form/Form.tsx
@@ -1,16 +1,66 @@
 import React from 'react';
 import styled from 'styled-components';
+import { offWhite, unit, trueBlack } from '../../styles';
 import { Heading } from '../Heading';
-import { unit } from '../../styles';
+import { Input } from '../Input';
+import { Text } from '../Text';
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 const Title = styled(Heading)`
   font-size: ${unit * 4}px;
 `;
 
+const Field = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: ${unit * 2}px 0;
+`;
+
+const Note = styled(Text)`
+  display: block;
+  position: relative;
+  padding: ${unit * 1.5}px ${unit * 3}px;
+  margin: ${unit * 3}px 0;
+
+  a {
+    color: ${trueBlack};
+    font-weight: bold;
+  }
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    background-color: blue;
+    width: 1px;
+  }
+`;
+
+const Label = styled.label`
+  font-family: 'Cabin', sans-serif;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: bold;
+  padding: 0 0 ${unit}px ${unit * 3}px;
+`;
+
+const FormInput = styled(Input)`
+  padding: ${unit * 1.5}px ${unit * 3}px;
+  background-color: ${offWhite};
+`;
+
 export interface FormSubComponents {
   Title: typeof Title;
+  Field: typeof Field;
+  Label: typeof Label;
+  Input: typeof FormInput;
+  Note: typeof Note;
 }
 
 export interface FormProps {
@@ -24,3 +74,7 @@ export const Form: React.FC<FormProps> & FormSubComponents = ({
 }: FormProps) => <Container className={className}>{children}</Container>;
 
 Form.Title = Title;
+Form.Field = Field;
+Form.Label = Label;
+Form.Input = FormInput;
+Form.Note = Note;
