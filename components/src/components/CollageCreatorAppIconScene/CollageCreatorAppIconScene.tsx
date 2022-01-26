c7c808762e721864711b07946d3f5434232c38e1
Jon Brennecke <jpbrennecke@gmail.com>
2022-01-26 10:51:08 -0800

    remove commented out code

--- components/src/components/CollageCreatorAppIconScene/CollageCreatorAppIconScene.tsx
+++ components/src/components/CollageCreatorAppIconScene/CollageCreatorAppIconScene.tsx
@@ -16,42 +16,11 @@ async function loadModel(): Promise<THREE.Group> {
         : [mesh.material];
       mesh.castShadow = true;
       mesh.receiveShadow = true;
-      materials.forEach((material) => {
-        // material.shad
-      });
     }
-    //   if (object.name == "Body001_Screen_Glass_0") {
-    //     object.removeFromParent();
-    //   }
-
-    //   if (object.name === WALLPAPER_OBJECT_NAME && object instanceof THREE.Mesh) {
-    //     const mesh = object as THREE.Mesh;
-
-    //     const textureLoader = new THREE.TextureLoader();
-    //     const texture = await textureLoader.loadAsync(imageSource);
-    //     texture.encoding = THREE.sRGBEncoding;
-
-    //     // @ts-ignore
-    //     const material: THREE.MeshStandardMaterial | null = mesh.material;
-    //     if (material && material instanceof THREE.Material) {
-    //       material.map = texture;
-    //       material.emissiveMap = texture;
-    //       material.emissiveIntensity = 2;
-    //       material.flatShading = true;
-    //       material.blending = THREE.NoBlending;
-    //       material.emissive = new THREE.Color(0x999999);
-    //       material.needsUpdate = true;
-    //     }
-    //   }
   });
 
-  // model.up = new THREE.Vector3(0, 0, 1);
   model.rotation.x = Math.PI * 0.5;
   model.position.set(0, 0, 0);
-  // model.scale.set(5, 5, 5);
-  // model.rotation.y = IPHONE_INITIAL_ROTATION_Y;
-  // model.rotation.x = -Math.PI * 0.05;
-  // model.rotation.z = Math.PI * 0.05;
 
   return model;
 }
@@ -77,10 +46,8 @@ export function CollageCreatorAppIconScene({
       if (!canvas) {
         return;
       }
-
       const model = await loadModel();
       setModel(model);
-
       setUpScene(model, canvas);
     }
 
