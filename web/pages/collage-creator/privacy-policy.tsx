545713f90183dc1fcf40e6706701b73133f855fc
Jon Brennecke <jpbrennecke@gmail.com>
2022-01-26 19:36:57 -0800

    add tailwind, tailwind/typography and get privacy policy on server

--- web/pages/collage-creator/privacy-policy.tsx
+++ web/pages/collage-creator/privacy-policy.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useRef } from 'react';
-import { Page } from '../../src/components';
-import styled from 'styled-components';
 import { maxWidth } from '@portfolio/components';
+import React from 'react';
+import styled from 'styled-components';
+import { Page } from '../../src/components';
 
 const BodyContainer = styled.div`
   max-width: ${maxWidth}px;
@@ -10,31 +10,39 @@ const BodyContainer = styled.div`
 const IUBENDA_PRIVACY_POLICY_URL =
   'https://www.iubenda.com/api/privacy-policy/70209080/no-markup';
 
-export default function CollageCreatorPrivacyPolicyPage() {
-  const policyContainerRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    async function asyncEffect() {
-      const result = await fetch(IUBENDA_PRIVACY_POLICY_URL);
-      if (result.status !== 200) {
-        return;
-      }
-      const { content } = await result.json();
-      if (policyContainerRef.current) {
-        policyContainerRef.current.innerHTML = content;
-      }
-    }
-
-    asyncEffect();
-  }, []);
-
+export default function CollageCreatorPrivacyPolicyPage({
+  privacyPolicyHTML,
+}: {
+  privacyPolicyHTML: string;
+}) {
   return (
-    <Page contentStyle="dark">
+    <Page contentStyle="dark" showLoadingBanner={false}>
       {() => (
         <BodyContainer>
-          <div ref={policyContainerRef} />
+          <article
+            className="prose mx-auto mt-32"
+            dangerouslySetInnerHTML={{ __html: privacyPolicyHTML }}
+          />
         </BodyContainer>
       )}
     </Page>
   );
 }
+
+async function fetchPrivacyPolicy(): Promise<string> {
+  const result = await fetch(IUBENDA_PRIVACY_POLICY_URL);
+  if (result.status !== 200) {
+    throw new Error(
+      `Failed to fetch privacy policy from ${IUBENDA_PRIVACY_POLICY_URL}`
+    );
+  }
+  const { content } = await result.json();
+  return content;
+}
+
+CollageCreatorPrivacyPolicyPage.getInitialProps = async () => {
+  const privacyPolicyHTML = await fetchPrivacyPolicy();
+  return {
+    privacyPolicyHTML,
+  };
+};
